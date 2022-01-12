758ca182e8adb54a1bae9da915180837c1f8474d
DTHNhung <damthihongnhung2ktn@gmail.com>
2022-01-12 09:31:25 +0700

    update sql

--- mini_project.sql
+++ mini_project.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th1 08, 2022 lúc 03:45 PM
--- Phiên bản máy phục vụ: 10.4.20-MariaDB
--- Phiên bản PHP: 7.3.29
+-- Host: 127.0.0.1
+-- Generation Time: Jan 11, 2022 at 03:36 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,86 +18,56 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Cơ sở dữ liệu: `mini_project`
+-- Database: `mini_project`
 --
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `tbl_admin`
---
-
-CREATE TABLE `tbl_admin` (
-  `admin_id` int(11) NOT NULL,
-  `admin_email` varchar(255) NOT NULL,
-  `admin_password` varchar(255) NOT NULL,
-  `admin_name` varchar(255) DEFAULT NULL,
-  `admin_avatar` varchar(255) DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Đang đổ dữ liệu cho bảng `tbl_admin`
---
-
-INSERT INTO `tbl_admin` (`admin_id`, `admin_email`, `admin_password`, `admin_name`, `admin_avatar`, `created_at`, `updated_at`) VALUES
-(1, '2', '3', '4', '5', '2022-01-18 01:10:54', '0000-00-00 00:00:00');
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `tbl_users`
+-- Table structure for table `tbl_users`
 --
 
 CREATE TABLE `tbl_users` (
   `user_id` int(11) NOT NULL,
   `user_email` varchar(255) NOT NULL,
   `user_password` varchar(255) NOT NULL,
-  `user_name` varchar(255) DEFAULT NULL,
-  `user_avatar` varchar(255) DEFAULT NULL,
+  `user_name` varchar(100) NOT NULL,
+  `user_avatar` varchar(255) NOT NULL,
   `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL
+  `updated_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Đang đổ dữ liệu cho bảng `tbl_users`
+-- Dumping data for table `tbl_users`
 --
 
 INSERT INTO `tbl_users` (`user_id`, `user_email`, `user_password`, `user_name`, `user_avatar`, `created_at`, `updated_at`) VALUES
-(6, 'khieutrung12@gmail.com', '6c7fafe5183d45bbb5df575ae54549b8', 'thaihung', NULL, NULL, NULL);
+(20, 'nhunga817@gmail.com', 'd8df5559dd070f28933c3628b4214382', 'hongnhung', '61dbf69d6b7c16.84987261.jpg', '2022-01-10 16:04:29', '2022-01-10 16:04:29'),
+(21, 'damthihongnhung2kTN@gmail.com', 'd8df5559dd070f28933c3628b4214382', 'admin001', '61dc0571d41f83.32956962.jpg', '2022-01-10 17:07:45', '2022-01-10 17:07:45'),
+(22, 'damthihongnhung_t63@hus.edu.vn', 'd8df5559dd070f28933c3628b4214382', 'admin002', '61dc058a04ae17.15194549.jpg', '2022-01-10 17:08:10', '2022-01-10 17:08:10'),
+(24, 'dthnhung1701@gmail.com', 'd8df5559dd070f28933c3628b4214382', 'admin', '61dd515acd3d27.64515326.jpg', '2022-01-11 16:43:54', '2022-01-11 16:43:54');
 
 --
--- Chỉ mục cho các bảng đã đổ
+-- Indexes for dumped tables
 --
 
 --
--- Chỉ mục cho bảng `tbl_admin`
---
-ALTER TABLE `tbl_admin`
-  ADD PRIMARY KEY (`admin_id`);
-
---
--- Chỉ mục cho bảng `tbl_users`
+-- Indexes for table `tbl_users`
 --
 ALTER TABLE `tbl_users`
-  ADD PRIMARY KEY (`user_id`);
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `user_email` (`user_email`),
+  ADD UNIQUE KEY `user_name` (`user_name`);
 
 --
--- AUTO_INCREMENT cho các bảng đã đổ
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT cho bảng `tbl_admin`
---
-ALTER TABLE `tbl_admin`
-  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT cho bảng `tbl_users`
+-- AUTO_INCREMENT for table `tbl_users`
 --
 ALTER TABLE `tbl_users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
